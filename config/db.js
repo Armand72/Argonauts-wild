f75d42ad5181ac1e03233ac33b7b38d913dde695
Armand72 <armandmeunier@hotmail.fr>
2020-08-12 19:41:16 +0200

    added uri

--- config/db.js
+++ config/db.js
@@ -4,13 +4,10 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://armand72:armand72@cluster0.c6qyu.mongodb.net/<dbname>?retryWrites=true&w=majority",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("MongoDB connected");
   } catch (err) {
