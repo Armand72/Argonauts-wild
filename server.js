e62c6801b0e9e6812404309b26613ac61371fe50
Armand72 <armandmeunier@hotmail.fr>
2020-08-12 10:26:10 +0200

    fix bug

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || SERVER_ADDRESS;
+const port = process.env.PORT || 4000;
 
 const server = require("http").createServer(app);
 server.listen(port, () => {
